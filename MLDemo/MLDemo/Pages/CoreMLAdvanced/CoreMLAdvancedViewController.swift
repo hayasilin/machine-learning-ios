c74a8462c884164de81de11762b15e128a9c3407
hayasilin <harvey.hayasi@gmail.com>
2020-03-14 23:18:48 +0800

    Add face detection

--- MLDemo/MLDemo/Pages/CoreMLAdvanced/CoreMLAdvancedViewController.swift
+++ MLDemo/MLDemo/Pages/CoreMLAdvanced/CoreMLAdvancedViewController.swift
@@ -8,23 +8,88 @@
 
 import UIKit
 
-class CoreMLAdvancedViewController: UIViewController {
+class CoreMLAdvancedViewController: UITableViewController {
+
+enum SettingsFunctions {
+        case faceDetection
+        case barcodeDetection
+        case textDetection
+        case horizonDetection
+        case objectTracking
+        case naturalLanguage
+
+        var title: String? {
+            switch self {
+            case .faceDetection:
+                return "faceDetection"
+            case .barcodeDetection:
+                return "barcodeDetection"
+            case .textDetection:
+                return "textDetection"
+            case .horizonDetection:
+                return "horizonDetection"
+            case .objectTracking:
+                return "objectTracking"
+            case .naturalLanguage:
+                return "naturalLanguage"
+            }
+        }
+    }
+
+    lazy var dataSource: [SettingsFunctions] = [
+        .faceDetection,
+        .barcodeDetection,
+        .textDetection,
+        .horizonDetection,
+        .objectTracking,
+        .naturalLanguage,
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
     }
-    */
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self), for: indexPath)
+
+        cell.textLabel?.text = dataSource[indexPath.row].title
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        var viewController: UIViewController = UIViewController()
+
+        switch dataSource[indexPath.row] {
+        case .faceDetection:
+            viewController = FaceDetectionViewController()
+
+        case .barcodeDetection:
+            viewController = CaptureClassificationViewController()
+
+        case .textDetection:
+            viewController = ImageSimilarityViewController()
+
+        case .horizonDetection:
+            viewController = ObjectDetectionViewController()
+
+        case .objectTracking:
+            viewController = StyleTransferViewController()
+
+        case .naturalLanguage:
+            viewController = TextClassificationViewController()
+        }
+
+        navigationController?.pushViewController(viewController, animated: true)
+    }
 }
